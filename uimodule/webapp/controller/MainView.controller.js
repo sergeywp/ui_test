d23df2bf7e5c3ef6d74d4301dc6fd5f450833115
Sergey Ponomarenko <sergeywp92@yandex.ru>
2021-09-19 09:16:30 +0500

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["demo/ui/ui5Demo/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("demo.ui.ui5Demo.controller.MainView", {});
+});
